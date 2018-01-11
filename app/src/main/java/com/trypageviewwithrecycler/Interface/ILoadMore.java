89972ee24ac945b991472ea10efd626aca880c62
Julia1632 <julia.zhurun6552817w@gmail.com>
2018-01-11 12:39:59 +0300

    Add d r

---
+++ app/src/main/java/com/trypageviewwithrecycler/Interface/ILoadMore.java
@@ -0,0 +1,9 @@
+package com.trypageviewwithrecycler.Interface;
+
+/**
+ * Created by zigin on 11.10.2017.
+ */
+
+public interface ILoadMore {
+    void onLoadMore();
+}
--- app/src/main/java/com/trypageviewwithrecycler/SecondFragment.java
+++ app/src/main/java/com/trypageviewwithrecycler/SecondFragment.java
@@ -2,19 +2,27 @@ package com.trypageviewwithrecycler;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.trypageviewwithrecycler.Adapter.MyAdapter;
 import com.trypageviewwithrecycler.Info.Info;
-import com.trypageviewwithrecycler.Info.InfoRecycleAdapter;
+import com.trypageviewwithrecycler.Interface.ILoadMore;
+import com.trypageviewwithrecycler.Model.Item;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by User on 10.01.2018.
@@ -25,6 +33,8 @@ public class SecondFragment extends Fragment {
     private String title;
     private int page;
     private List<Info> info10;
+    List<Item> items = new ArrayList<>();
+    MyAdapter adapter;
 
     public static SecondFragment newInstance(int page, String title) {
         SecondFragment fragmentFirst = new SecondFragment();
@@ -49,38 +59,60 @@ public class SecondFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        final View view = inflater.inflate(R.layout.fragment_second, container, false);
         TextView tvLabel = view.findViewById(R.id.tvLabel);
         tvLabel.setText(page + " -- " + title);
-        init();
-        RecyclerView rv = view.findViewById(R.id.recyclerView);
+        //init();
+        //- RecyclerView rv = view.findViewById(R.id.recyclerView);
         // rv.setHasFixedSize(true);
-        rv.setLayoutManager(new LinearLayoutManager(view.getContext(), LinearLayoutManager.VERTICAL, false));
-        rv.setAdapter(new InfoRecycleAdapter(info10));
+        // rv.setLayoutManager(new LinearLayoutManager(view.getContext(), LinearLayoutManager.VERTICAL, false));
+        //rv.setAdapter(new InfoRecycleAdapter(info10));
+        random10Data(0, 10);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        Log.e(TAG, "onCreateView: recycle" );
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        adapter = new MyAdapter(recyclerView, view.getContext(), items);
+        recyclerView.setAdapter(adapter);
+
+        //Set Load more
+        adapter.setLoadMore(new ILoadMore() {
+            @Override
+            public void onLoadMore() {
+                if (items.size() <= 100) {
+                    items.add(null);
+                    adapter.notifyItemInserted(items.size() - 1);
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            items.remove(items.size() - 1);
+                            adapter.notifyItemRemoved(items.size());
+
+                            //random more data
+                            int index = items.size();
+                            int end = index + 10;
+                            random10Data(index, end);
+                            adapter.notifyDataSetChanged();
+                            adapter.setLoaded();
+                        }
+                    }, 3000);
+                } else {
+                    Toast.makeText(view.getContext(), "Load data completed !!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
         return view;
     }
 
-    private void init() {
-        info10 = new ArrayList<>();
-        info10.add(new Info("1name", "1s"));
-        info10.add(new Info("2name", "2s"));
-        info10.add(new Info("3name", "3s"));
-        info10.add(new Info("4name", "4s"));
-        info10.add(new Info("5name", "5s"));
-        info10.add(new Info("6name", "6s"));
-        info10.add(new Info("7name", "7s"));
-        info10.add(new Info("8name", "8s"));
-        info10.add(new Info("9name", "9s"));
-        info10.add(new Info("1name", "1s"));
-        info10.add(new Info("2name", "2s"));
-        info10.add(new Info("3name", "3s"));
-        info10.add(new Info("4name", "4s"));
-        info10.add(new Info("5name", "5s"));
-        info10.add(new Info("6name", "6s"));
-        info10.add(new Info("7name", "7s"));
-        info10.add(new Info("8name", "8s"));
-        info10.add(new Info("9name", "9s"));
-
-
+    private void random10Data(int indexStart, int indexEnd) {
+        //random data
+        for (int i = indexStart; i < indexEnd; i++) {
+            String name = UUID.randomUUID().toString();
+            Item newItem = new Item(name, name.length());
+            items.add(newItem);
+        }
     }
 }
